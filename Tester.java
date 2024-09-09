8fc1458a8ac67a190e8ef389627394a5c0a8fd66
Dewangga29-firmansyah <dewangga_firmansyah_33rpl@student.smktelkom-mlg.sch.id>
2024-09-09 14:30:44 +0700

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,11 @@
+package toko;
+
+public class Tester {
+
+    public static void main(String[] args) {
+        //Buat Object Class dari Driver Blass berikut
+        CD c1 = new CD();
+        c1.print();
+    }
+    
+}
